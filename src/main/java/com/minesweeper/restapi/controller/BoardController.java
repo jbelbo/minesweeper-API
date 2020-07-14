13cb1a3662ed7096e7512f95fabb818012c4b23d
mspinarolli <spinarolli.matias@gmail.com>
2020-07-14 18:24:58 -0300

    Board creation and persistence using endpoint is now working

--- src/main/java/com/minesweeper/restapi/controller/BoardController.java
+++ src/main/java/com/minesweeper/restapi/controller/BoardController.java
@@ -2,6 +2,8 @@ package com.minesweeper.restapi.controller;
 
 import com.minesweeper.restapi.model.Board;
 import com.minesweeper.restapi.model.BoardParameters;
+import com.minesweeper.restapi.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -11,6 +13,9 @@ import java.util.UUID;
 @RequestMapping(value = "/api", produces = "application/json")
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
     @GetMapping("/boards/{boardId}")
     public Board getBoardById(@PathVariable UUID boardId) {
         return new Board();
@@ -18,7 +23,7 @@ public class BoardController {
 
     @PostMapping("/boards")
     public Board createBoard(@RequestBody BoardParameters boardParameters) {
-        return new Board(boardParameters);
+        return boardService.createBoard(boardParameters);
     }
 
     @PutMapping("/boards/{boardId}/cells/{cellId}")
