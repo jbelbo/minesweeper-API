9a014f6da5c40912d7c71054bb3b76c8be1be791
mspinarolli <spinarolli.matias@gmail.com>
2020-07-18 09:24:58 -0300

    Execution of event and board persistance

--- src/main/java/com/minesweeper/restapi/service/BoardService.java
+++ src/main/java/com/minesweeper/restapi/service/BoardService.java
@@ -1,5 +1,6 @@
 package com.minesweeper.restapi.service;
 
+import com.minesweeper.restapi.event.UpdateCellEvent;
 import com.minesweeper.restapi.model.Board;
 import com.minesweeper.restapi.model.BoardParameters;
 import com.minesweeper.restapi.repository.BoardRepository;
@@ -20,6 +21,20 @@ public class BoardService {
     }
 
     public Board getBoardById(UUID boardId) {
+        return getByIdOrThrowNotFound(boardId);
+    }
+
+    public Board applyUpdateCellEvent(UpdateCellEvent event) {
+        var board = getByIdOrThrowNotFound(event.getBoardId());
+        // ToDo check board status before applying any event
+        event.apply(board);
+        boardRepository.save(board);
+
+        return board;
+    }
+
+    // ToDo create custom method in repository
+    private Board getByIdOrThrowNotFound(UUID boardId) {
         return boardRepository.findById(boardId).orElseThrow(() -> new EntityNotFoundException("Board with id " + boardId.toString() + " not found"));
     }
 }
