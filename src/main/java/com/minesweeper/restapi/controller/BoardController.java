7894c8523dab160d968c5025f67fce99b661e804
mspinarolli <spinarolli.matias@gmail.com>
2020-07-18 09:22:19 -0300

    New endpoint implemented

--- src/main/java/com/minesweeper/restapi/controller/BoardController.java
+++ src/main/java/com/minesweeper/restapi/controller/BoardController.java
@@ -1,11 +1,15 @@
 package com.minesweeper.restapi.controller;
 
+import com.minesweeper.restapi.event.*;
 import com.minesweeper.restapi.model.Board;
 import com.minesweeper.restapi.model.BoardParameters;
+import com.minesweeper.restapi.model.UpdateCellParameters;
 import com.minesweeper.restapi.service.BoardService;
+import org.hibernate.sql.Update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.InvalidParameterException;
 import java.util.UUID;
 
 // ToDo implement methods using persistence or in-memory list
@@ -26,8 +30,16 @@ public class BoardController {
         return boardService.createBoard(boardParameters);
     }
 
-    @PutMapping("/boards/{boardId}/cells/{cellId}")
-    public Board updateCell(@PathVariable UUID boardId, @PathVariable UUID cellId) {
-        return new Board();
+    @PostMapping("/boards/{boardId}/events")
+    public Board updateCell(@PathVariable UUID boardId, @RequestBody UpdateCellParameters parameters) throws Exception {
+        UpdateCellEvent event;
+        switch (parameters.type) {
+            case REVEAL -> event = new RevealCellEvent(boardId, parameters.cellId);
+            case ADD_FLAG -> event = new AddFlagCellEvent(boardId, parameters.cellId);
+            case ADD_QUESTION_MARK -> event = new AddQuestionMarkCellEvent(boardId, parameters.cellId);
+            default -> throw new Exception("Invalid event type");
+        }
+
+        return boardService.applyUpdateCellEvent(event);
     }
 }
