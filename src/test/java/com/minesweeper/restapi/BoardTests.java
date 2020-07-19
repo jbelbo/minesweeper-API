fe8d5bcdc5c50d2e52cc278d40825cb8993b5e81
mspinarolli <spinarolli.matias@gmail.com>
2020-07-19 17:54:08 -0300

    More Board tests added

--- src/test/java/com/minesweeper/restapi/BoardTests.java
+++ src/test/java/com/minesweeper/restapi/BoardTests.java
@@ -2,8 +2,9 @@ package com.minesweeper.restapi;
 
 import com.minesweeper.restapi.model.Board;
 import com.minesweeper.restapi.model.BoardParameters;
+import com.minesweeper.restapi.model.BoardStatus;
+import com.minesweeper.restapi.model.Cell;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -34,8 +35,35 @@ public class BoardTests {
         assertThat(board.getNumberOfMines() == boardParameters.numberOfMines);
     }
 
+    @ParameterizedTest
+    @MethodSource("provideParametersForBoardCreation")
+    public void revealCell_cellHasMine_returnsBoardWithStatusLost(BoardParameters boardParameters) {
+        var board = new Board(boardParameters);
+        var cellWithMine = board.getCells().stream()
+                .filter(Cell::hasMine).findFirst();
+
+        board.revealCell(cellWithMine.get().getId());
+
+        assertThat(BoardStatus.LOST == board.getStatus());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParametersForBoardCreation")
+    public void revealCell_allCellsWithoutMinesRevealed_returnsBoardWithStatusWon(BoardParameters boardParameters) {
+        var board = new Board(boardParameters);
+
+        board.getCells().forEach(cell -> {
+            if (!cell.hasMine()) {
+                board.revealCell(cell.getId());
+            }
+        });
+
+        assertThat(BoardStatus.WON == board.getStatus());
+    }
+
     private static Stream<Arguments> provideParametersForBoardCreation() {
         return Stream.of(
+                Arguments.of(new BoardParameters(8,8, 15)),
                 Arguments.of(new BoardParameters(4,3, 2)),
                 Arguments.of(new BoardParameters(1,1,1)),
                 Arguments.of(new BoardParameters(10,20,30))
