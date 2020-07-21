13804157f8a34025a159fdd7c34abde83c2a0f71
mspinarolli <spinarolli.matias@gmail.com>
2020-07-21 10:15:56 -0300

    Method findByUsername added to BoardRepository

--- src/main/java/com/minesweeper/restapi/repository/BoardRepository.java
+++ src/main/java/com/minesweeper/restapi/repository/BoardRepository.java
@@ -3,7 +3,10 @@ package com.minesweeper.restapi.repository;
 import com.minesweeper.restapi.model.Board;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BoardRepository extends CrudRepository<Board, UUID> {
+
+    public List<Board> findByUsername(String username);
 }
